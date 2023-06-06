c7cf69260fb217c80a769da2a681cd10f0a0b013
Jason Triemert <jaytriemert@gmail.com>
2023-06-06 14:33:48 -0500

    updated user model

--- server/models/User.js
+++ server/models/User.js
@@ -15,16 +15,3 @@ const userSchema = new Schema({
 
 module.exports = model('User', userSchema);
 
-// Comment Model
-const { Schema, model } = require('mongoose');
-
-const commentSchema = new Schema({
-  user_id: { type: Schema.Types.ObjectId, ref: 'User' },
-  trail_id: { type: Schema.Types.ObjectId, ref: 'Trail' },
-  comment_text: String,
-  is_public: Boolean,
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
-});
-
-module.exports = model('Comment', commentSchema);
