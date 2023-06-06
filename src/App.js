bbd9b7a656f63d6c59e4f9155316299213db169b
Mylan <moe1401@gmail.com>
2023-06-06 14:09:44 -0500

    changes to files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import NavTabs from './pages/NavTabs';
-import Form from './components/pages/Form/index';
-import Home from './pages/Home';
-// import Profile from './pages/Profile';
-// import Trails from './pages/Trails';
 import Container from "./components/Container";
 
 const App = () => <Container />;
