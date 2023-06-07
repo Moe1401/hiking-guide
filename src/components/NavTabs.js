29e8ec80935bc8f575c23d02f47360ad1b055a45
kilgette <kilgette@gmail.com>
2023-06-07 12:43:10 -0500

    fixed nav bullets

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
@@ -9,8 +7,6 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#home"
           onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
           Home
@@ -34,15 +30,6 @@ function NavTabs({ currentPage, handlePageChange }) {
           Trails
         </a>
       </li>
-      <li className="nav-item">
-        {/* <a
-          href="#login"
-          onClick={() => handlePageChange('Login')}
-          className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
-        >
-          Login
-        </a> */}
-      </li>      
     </ul>
   );
 }
