e75db7ac73d68927296dff1de9f36085b2a7c4ef
kilgette <kilgette@gmail.com>
2023-06-06 19:39:11 -0500

    create login

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,11 @@
 import React from 'react';
+const Login = () => {
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+
+
+
+}
 
 export default function Home() {
   return (
