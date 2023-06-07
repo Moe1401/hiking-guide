331523a080abf29b203c07038ecd3488266f9760
Jason Triemert <jaytriemert@gmail.com>
2023-06-07 13:58:42 -0500

    added close connection in seed

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -3,6 +3,7 @@ const seedUsers = require('./userSeed');
 const seedTrails = require('./trailSeed');
 const seedHikes = require('./hikeSeed');
 const seedComments = require('./commentsSeed');
+const { connection } = require('mongoose');
 
 async function seedData() {
   try {
@@ -13,8 +14,11 @@ async function seedData() {
     console.log('All seed data successfully executed!');
   } catch (error) {
     console.error('Error seeding data:', error);
+  } finally {
+    await connection.close();
   }
 }
 seedData();
+
 
 
