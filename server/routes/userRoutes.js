38335aac7948078f95d1dfa8558ee6ced4b40c6e
Ian Bui <thinkbui@gmail.com>
2023-06-08 10:51:40 -0500

    Added route to pull user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,8 +9,8 @@ const {
 
 router.route('/user/login').post(createUser);
 
-// // /api/students/:studentId
-// router.route('/:studentId').get(getSingleStudent).delete(deleteStudent);
+// /api/students/:studentId
+router.route('/user/:id').get(getUserById);
 
 // // /api/students/:studentId/assignments
 // router.route('/:studentId/assignments').post(addAssignment);
