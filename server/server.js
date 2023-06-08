c7173f5261cb7fbf011861c697c13ea4c5795581
kilgette <kilgette@gmail.com>
2023-06-08 09:39:30 -0500

    loginsetup items

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ const path = require('path');
 // const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 // const server = new ApolloServer({
 //   typeDefs,
 //   resolvers,
