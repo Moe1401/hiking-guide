263f7953cc2f0afe4f447498ecffe345bd35b956
Ian Bui <thinkbui@gmail.com>
2023-06-09 01:06:14 -0500

    Added map links

--- client/src/components/pages/Profile/ProfileHike.js
+++ client/src/components/pages/Profile/ProfileHike.js
@@ -1,9 +1,17 @@
+import {gMap, sentinelMap} from "../../../utils/maps"
+
 function ProfileHike(props) {
   return(
     <div style={{border: "1px solid red"}}>
       <p>Trail: {props.hike.trail_id}</p>
       <p>Goal: {props.hike.goal_distance}</p>
       <p>Distance Hiked: {props.hike.distance}</p>
+      <p>
+        <a href={gMap({})} target="_blank" rel="noreferrer noopener">Google Maps</a>
+      </p>
+      <p>
+        <a href={sentinelMap({})} target="_blank" rel="noreferrer noopener">Sentinel Maps</a>
+      </p>
     </div>
   )
 }
---
+++ client/src/utils/maps.js
@@ -0,0 +1,19 @@
+const lat = (settings = {}) => {
+  return settings.lat || 46.4303749;
+}
+
+const lon = (settings = {}) => {
+  return settings.lon || -94.6103113;
+}
+
+const zoom = (settings = {}) => {
+  return settings.zoom || 9;
+}
+
+export const gMap = (settings = {}) => {
+  return `https://www.google.com/maps/@${lat(settings)},${lon(settings)},${zoom(settings)}z`;
+}
+
+export const sentinelMap = (settings = {}) => {
+  return `https://apps.sentinel-hub.com/sentinel-playground/?source=S2L2A&lat=${lat(settings)}&lng=${lon(settings)}&zoom=${zoom(settings)}`;
+}
