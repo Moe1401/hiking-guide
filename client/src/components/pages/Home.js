67cb2f5303af8c6089f342c1f5e5cfa454c61a89
kilgette <kilgette@gmail.com>
2023-06-11 15:58:11 -0500

    homepage content tweaks

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,21 +4,18 @@ import './Home.css';
 
 export default function Home() {
   return (
-    <Container className = "content">
+    <Container className="content">
       <h1>Trail Blazer - Minnesota's Premiere Hiking App</h1>
-      <p>Are you an outdoors lover looking for the best hiking trails in Minnesota? Look no further! <br></br>
-        Our app is designed with adventurers like you in mind. 
-        
-        <p></p>Whether you enjoy hiking, 
-        biking, trail running, or exploring other outdoor activities, we've got you covered.</p>
+      <p>Are you an outdoors lover looking for the best hiking trails in Minnesota? Look no further! <br />
+        Our app is designed with adventurers like you in mind.</p>
 
-        <p>Browse trails or create an account and track the date and distance of your hike. <br>
-        Set distance goals and crush your fitness goals</br> </p>
+      <p>Whether you enjoy hiking, biking, trail running, or exploring other outdoor activities, we've got you covered.</p>
+
+      <p>Browse trails or create an account and track the date and distance of your hike. <br />
+        Set distance goals and crush your fitness goals. </p>
       <Row>
         <Col sm={8}>
-          <div className="form-container">
-            
-          </div>
+          <div className="form-container"></div>
         </Col>
       </Row>
     </Container>
