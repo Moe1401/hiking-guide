62a4fe9de77a539a715ad50cbf476e8c6eb1a180
kilgette <kilgette@gmail.com>
2023-06-11 15:54:31 -0500

    homepage content buildout

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,13 +4,18 @@ import './Home.css';
 
 export default function Home() {
   return (
-    <Container>
+    <Container className = "content">
       <h1>Trail Blazer - Minnesota's Premiere Hiking App</h1>
       <p>Are you an outdoors lover looking for the best hiking trails in Minnesota? Look no further! <br></br>
-        Our app is designed with adventurers like you in mind. <br></br>Whether you enjoy hiking, 
+        Our app is designed with adventurers like you in mind. 
+        
+        <p></p>Whether you enjoy hiking, 
         biking, trail running, or exploring other outdoor activities, we've got you covered.</p>
+
+        <p>Browse trails or create an account and track the date and distance of your hike. <br>
+        Set distance goals and crush your fitness goals</br> </p>
       <Row>
-        <Col sm={6}>
+        <Col sm={8}>
           <div className="form-container">
             
           </div>
