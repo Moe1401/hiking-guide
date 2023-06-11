df130e2864d9b05c1994741900f4e8be70dffb35
Ian Bui <thinkbui@gmail.com>
2023-06-10 23:03:41 -0500

    Added draft of OpenStreetMap util

--- client/src/utils/maps.js
+++ client/src/utils/maps.js
@@ -10,6 +10,14 @@ const zoom = (settings = {}) => {
   return settings.zoom || 9;
 }
 
+const embedWidth = (settings = {}) => {
+  return settings.embedWidth || 300;
+}
+
+const embedHeight = (settings = {}) => {
+  return settings.embedHeight || 300;
+}
+
 export const gMap = (settings = {}) => {
   return `https://www.google.com/maps/@${lat(settings)},${lon(settings)},${zoom(settings)}z`;
 }
@@ -21,3 +29,12 @@ export const openStreetMap = (settings = {}) => {
 export const sentinelMap = (settings = {}) => {
   return `https://apps.sentinel-hub.com/sentinel-playground/?source=S2L2A&lat=${lat(settings)}&lng=${lon(settings)}&zoom=${zoom(settings)}`;
 }
+
+// export const openStreetMapEmbed = (settings = {}) => {
+//   return `<iframe id="inlineFrameExample"
+//     title="Inline Frame Example"
+//     width="${embedWidth(settings)}"
+//     height="${embedHeight(settings)}"
+//     src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+//   </iframe>`
+// }
