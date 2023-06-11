fbfdc4ba41a8b600df11ddf2d31340f57031e044
Mylan <moe1401@gmail.com>
2023-06-11 18:52:56 -0500

    updated server.js with gary code

--- server/server.js
+++ server/server.js
@@ -11,12 +11,12 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use(express.static(path.join(__dirname, "..", 'client/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", 'client/build/index.html'))
+  })
+}
 
 db.once('open', () => {
   app.listen(PORT, () => {
