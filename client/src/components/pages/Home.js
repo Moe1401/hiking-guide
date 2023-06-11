63f1598c2cc06972f6a5e9ec376b3a96e83488f0
kilgette <kilgette@gmail.com>
2023-06-11 15:36:31 -0500

    home page content buildout

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -5,8 +5,10 @@ import './Home.css';
 export default function Home() {
   return (
     <Container>
-      <h1>Home Page</h1>
-      <p>Welcome home!</p>
+      <h1>Trail Blazer - Minnesota's Premiere Hiking App</h1>
+      <p>Are you an outdoors lover looking for the best hiking trails in Minnesota? Look no further! <br></br>
+        Our app is designed with adventurers like you in mind. <br></br>Whether you enjoy hiking, 
+        biking, trail running, or exploring other outdoor activities, we've got you covered.</p>
       <Row>
         <Col sm={6}>
           <div className="form-container">
