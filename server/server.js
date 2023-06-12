e34f0eaa2dfe30550a1f1c78002a1aea6d771e9a
Mylan <moe1401@gmail.com>
2023-06-12 09:42:00 -0500

    added fix

--- server/server.js
+++ server/server.js
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "..", 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", 'client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
   })
 }
 
